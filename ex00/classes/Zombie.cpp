5eed9188cb2105c3361a4ef1353c3971ef3c8577
dcresce <dcresce@student.42lausanne.com>
2026-09-08 14:21:50 +0200

    zombie class working

--- ex00/classes/Zombie.cpp
+++ ex00/classes/Zombie.cpp
@@ -5,7 +5,19 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcresce <dcresce@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 08/09/2026 13:50:25 by dcresce           #+#    #+#             */
-/*   Updated: 08/09/2026 13:50:25 by dcresce          ###   ########.ch       */
+/*   Created: 2026/09/08 14:11:42 by dcresce           #+#    #+#             */
+/*   Updated: 2026/09/08 14:16:57 by dcresce          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+//constructor
+Zombie::Zombie(std::string _name) : name(_name) {}
+//destructor
+Zombie::~Zombie() {};
+
+//Announce
+void	Zombie::announce(void) {
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
--- ex00/classes/Zombie.hpp
+++ ex00/classes/Zombie.hpp
@@ -5,7 +5,30 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcresce <dcresce@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/09/08 13:52:14 by dcresce           #+#    #+#             */
-/*   Updated: 2026/09/08 13:52:14 by dcresce          ###   ########.ch       */
+/*   Created: 2026/09/08 14:20:41 by dcresce           #+#    #+#             */
+/*   Updated: 2026/09/08 14:20:41 by dcresce          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+// include
+# include <string>
+# include <iostream>
+
+class Zombie {
+	private:
+		std::string name;
+
+	public:
+		//constructor
+		Zombie(std::string _name);
+		//destructor
+		~Zombie();
+		//announce
+		void	announce(void);
+
+};
+
+#endif
